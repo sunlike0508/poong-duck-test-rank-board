054526632a3206c2445c6a7ec980d22e5b64dbff
sunlike0508 <thesin1989@gmail.com>
2020-06-18 12:45:48 +0900

    테스크 코드에 맞는 메소드 생성

--- src/main/java/poongduck/board/controller/BoardController.java
+++ src/main/java/poongduck/board/controller/BoardController.java
@@ -1,7 +1,15 @@
 package poongduck.board.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import poongduck.board.entity.BoardEntity;
 
 @RestController
 public class BoardController {
@@ -9,5 +17,15 @@ public class BoardController {
 	@RequestMapping("/hello")
 	public String hello() {
 		return "Hello World";
+	}
+	
+	@RequestMapping(value="/board", method=RequestMethod.GET)
+	public ModelAndView openBoardList(ModelMap model) throws Exception{
+		ModelAndView mv = new ModelAndView("/board/boardList");
+		
+		List<BoardEntity> list = new ArrayList<BoardEntity>();
+		mv.addObject("list", list);
+		
+		return mv;
 	}
 }

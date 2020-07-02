26ba9cebca449d1ad45e944e2b43c1e46e590e01
sunlike0508 <thesin1989@gmail.com>
2020-07-02 16:52:39 +0900

    게시글 출력&등록 메소드 json으로 입출력하도록 변경

--- src/main/java/poongduck/board/controller/BoardController.java
+++ src/main/java/poongduck/board/controller/BoardController.java
@@ -3,42 +3,34 @@ package poongduck.board.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import poongduck.board.entity.BoardEntity;
 import poongduck.board.service.BoardService;
 
-@Controller
+@RestController
 public class BoardController {
 	
 	public static final String BOARD_LIST_URL = "/board";
-	public static final String BOARD_LIST_VIEW  = "/board/list";
-	public static final String BOARD_LIST_ATTRIBUTE = "boardList";
 	public static final String BOARD_WRITE_URL = "/board/write";
-	public static final String BOARD_LIST_REDIRECT_URL = "redirect:/board";
 	
 	@Autowired
 	private BoardService boardService;
 	
-	@RequestMapping(value=BOARD_LIST_URL, method=RequestMethod.GET)
-	public ModelAndView openBoardList(ModelMap model) throws Exception{
-		ModelAndView mv = new ModelAndView(BOARD_LIST_VIEW);
-
-		List<BoardEntity> boardList = boardService.selectBoardList();
-		mv.addObject(BOARD_LIST_ATTRIBUTE, boardList);
+	@GetMapping(BOARD_LIST_URL)
+	public List<BoardEntity> openBoardList() throws Exception{
 		
-		return mv;
+		return boardService.selectBoardList();
 	}
 	
-	@RequestMapping(value=BOARD_WRITE_URL, method=RequestMethod.POST)
-	public String writeBoard(BoardEntity board) throws Exception{
-		
+	@PostMapping(path = BOARD_WRITE_URL, produces = "application/json;charset=UTF-8")
+	public List<BoardEntity> writeBoard(@RequestBody BoardEntity board) throws Exception{
+
 		boardService.saveBoard(board);
 		
-		return BOARD_LIST_REDIRECT_URL;
+		return boardService.selectBoardList();
 	}
 }
--- src/test/java/poongduck/board/controller/BoardControllerTest.java
+++ src/test/java/poongduck/board/controller/BoardControllerTest.java
@@ -3,66 +3,71 @@ package poongduck.board.controller;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
+import java.nio.charset.Charset;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Properties;
 
-import org.dbunit.Assertion;
-import org.dbunit.DatabaseUnitException;
-import org.dbunit.database.DatabaseConfig;
-import org.dbunit.database.IDatabaseConnection;
-import org.dbunit.dataset.DataSetException;
-import org.dbunit.dataset.ITable;
-import org.dbunit.dataset.ReplacementDataSet;
-import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
-import org.dbunit.ext.mysql.MySqlConnection;
-import org.dbunit.operation.DatabaseOperation;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.unitils.reflectionassert.ReflectionComparatorMode;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import net.javacrumbs.jsonunit.core.Option;
 import poongduck.board.entity.BoardEntity;
 
-import static org.unitils.reflectionassert.ReflectionAssert.assertReflectionEquals;
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+
+import org.dbunit.DatabaseUnitException;
+import org.dbunit.database.DatabaseConfig;
+import org.dbunit.database.IDatabaseConnection;
+import org.dbunit.dataset.xml.FlatXmlDataSetBuilder;
+import org.dbunit.ext.mysql.MySqlConnection;
+import org.dbunit.operation.DatabaseOperation;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
 @AutoConfigureMockMvc
 class BoardControllerTest {
 	
-	private static final String PROPERTIES = "application.properties";
-	private static final String DRIVER = "spring.datasource.driver-class-name";
-	private static final String URL = "spring.datasource.url";
-	private static final String USERNAME = "spring.datasource.username";
-	private static final String PASSWARD = "spring.datasource.password";
-	private static final String SCHEMA = "spring.datasource.hikari.schema";
+	public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), 
+																MediaType.APPLICATION_JSON.getSubtype(), 
+																Charset.forName("UTF-8"));
+
+	private static final String APPLICATION_PROPERTIES = "application.properties";
 	
-	private static final String TABLE_BOARD = "board";
+	private static final String DRIVER_CLASS_NAME = "spring.datasource.driver-class-name";
+	private static final String DRIVER_CLASS_URL = "spring.datasource.url";
+	private static final String DRIVER_CLASS_USERNAME = "spring.datasource.username";
+	private static final String DRIVER_CLASS_PASSWORD = "spring.datasource.password";
+	private static final String DRIVER_CLASS_SCHEMA = "spring.datasource.hikari.schema";
 	
-	private static final String ID = "id";
-	private static final String USER_ID = "user_id";
-	private static final String CONTENTS = "contents";
+	private static final String JSON_IGNORE_ID = "[*].id";
+	private static final String JSON_IGNORE_CREATE_AT = "[*].create_at";
+	private static final String JSON_IGNORE_UPDATE_AT = "[*].update_at";
+	
+	private static final String BOARD_JSON = "board.json";
+	private static final String BOARD_XMl = "src/main/resources/Board.xml";
+	
+	private ObjectMapper objectMapper = new ObjectMapper();
 
 	@Autowired
 	MockMvc mockMvc;
@@ -71,19 +76,20 @@ class BoardControllerTest {
 
 	@BeforeEach
 	public void setUp() throws ClassNotFoundException, DatabaseUnitException, SQLException, IOException{
-		InputStream inputStream = getClass().getClassLoader().getResourceAsStream(PROPERTIES);
 		Properties properties = new Properties();
-		properties.load(inputStream);
+		properties.load(getClass().getClassLoader().getResourceAsStream(APPLICATION_PROPERTIES));
 		
-		Class.forName(properties.getProperty(DRIVER));
-		iDatabaseConnection = new MySqlConnection(
-				DriverManager.getConnection(properties.getProperty(URL), properties.getProperty(USERNAME), properties.getProperty(PASSWARD)),
-				properties.getProperty(SCHEMA));
+		final String dirver = properties.getProperty(DRIVER_CLASS_NAME);
+		final String url = properties.getProperty(DRIVER_CLASS_URL);
+		final String username = properties.getProperty(DRIVER_CLASS_USERNAME);
+		final String passward = properties.getProperty(DRIVER_CLASS_PASSWORD);
+		final String schema = properties.getProperty(DRIVER_CLASS_SCHEMA);
+		
+		Class.forName(dirver);
+		iDatabaseConnection = new MySqlConnection(DriverManager.getConnection(url, username, passward), schema);
 		iDatabaseConnection.getConfig().setProperty(DatabaseConfig.FEATURE_QUALIFIED_TABLE_NAMES, false);
-		
-		ReplacementDataSet dataSet = setRepalcementDataSet("Board.xml");
-		
-		DatabaseOperation.CLEAN_INSERT.execute(iDatabaseConnection, dataSet);
+
+		DatabaseOperation.CLEAN_INSERT.execute(iDatabaseConnection, new FlatXmlDataSetBuilder().build(new File(BOARD_XMl)));
     }
 
 	@AfterEach
@@ -92,79 +98,59 @@ class BoardControllerTest {
 			iDatabaseConnection.close();
 		}
 	}
-
-	@Test
-	public void testIDatabaseTester() throws Exception {
-		//when
-		ITable actualTable = iDatabaseConnection.createDataSet().getTable(TABLE_BOARD);
-		//expected
-		ITable expectedTable = setRepalcementDataSet("Expected_Board.xml").getTable(TABLE_BOARD);
-		//except comparison column
-		String[] ignoreCols = {"create_at", "update_at"};
-		Assertion.assertEqualsIgnoreCols(expectedTable, actualTable, ignoreCols);
-	}
-
+	
 	@Test
 	@DisplayName("게시글 리스트 출력 메소드 테스트")
 	public void testOpenBoardList() throws Exception {
 		
-    	//when : openBoardList 메소드를 실행하여 list 화면에 결과 값을 출력 할때
-    	MvcResult result = mockMvc.perform(get(BoardController.BOARD_LIST_URL))
-    			.andExpect(status().isOk())
-    			.andExpect(view().name(BoardController.BOARD_LIST_VIEW))
-    			.andExpect(model().attributeExists(BoardController.BOARD_LIST_ATTRIBUTE))
-    			.andDo(print())
-    			.andReturn();
-    	
-    	//그 결과 값이 boardList라고 하자.
-    	Object actualList = result.getModelAndView().getModel().get(BoardController.BOARD_LIST_ATTRIBUTE);
-    	
-    	//expected : boardList 변수 값은 다음 expectList 변수와 값이 같아야 한다.
-    	assertReflectionEquals(expectedBoardList(), actualList, ReflectionComparatorMode.LENIENT_DATES);
+    	//when
+    	MvcResult mockMVcResult = mockMvc.perform(get(BoardController.BOARD_LIST_URL)
+						    			.accept(APPLICATION_JSON_UTF8))
+						    			.andExpect(status().isOk())
+						    			.andDo(print())
+						    			.andReturn();
+		
+    	//then
+		assertThatJson(mockMVcResult.getResponse().getContentAsString())
+						.whenIgnoringPaths(JSON_IGNORE_ID, JSON_IGNORE_CREATE_AT, JSON_IGNORE_UPDATE_AT)
+						.when(Option.IGNORING_ARRAY_ORDER)
+						.isEqualTo(objectMapper.readValue(
+								getClass().getClassLoader().getResourceAsStream(BOARD_JSON), BoardEntity[].class));
     }
 
 	@Test
 	@DisplayName("게시글 작성 메소드 테스트")
 	public void testWriteBoard() throws Exception {
 		
-		mockMvc.perform(post(BoardController.BOARD_WRITE_URL)
-    			.param(USER_ID, "sunlike0303")
-    			.param(CONTENTS, "호드를 위하여"))
-    			.andExpect(redirectedUrl(BoardController.BOARD_LIST_URL))
-    			.andExpect(status().is3xxRedirection())
-    			.andExpect(view().name(BoardController.BOARD_LIST_REDIRECT_URL))
-    			.andDo(print());
-    }
-	
-	public ReplacementDataSet setRepalcementDataSet(String fileName) throws MalformedURLException, DataSetException {
+		//given
+		BoardEntity givenBoardEntity = makeGivenBoardEntity();
 		
-		ReplacementDataSet dataSet = new ReplacementDataSet(new FlatXmlDataSetBuilder().build(new File(fileName)));
-		
-		dataSet.addReplacementObject("[null]", null);
-		dataSet.addReplacementObject("[date]", new Date(System.currentTimeMillis()));
-		
-		return dataSet;
-	}
-	
-	public List<BoardEntity> expectedBoardList() throws DataSetException, MalformedURLException {
-		
-		List<BoardEntity> expectList = new ArrayList<BoardEntity>();
-    	ITable expectedTable = setRepalcementDataSet("Expected_Board_List.xml").getTable(TABLE_BOARD);
+		//when
+		MvcResult mockMVcResult = mockMvc.perform(post(BoardController.BOARD_WRITE_URL)
+										.content(objectMapper.writeValueAsString(givenBoardEntity))
+										.contentType(APPLICATION_JSON_UTF8))
+										.andExpect(status().isOk())
+						    			.andDo(print())
+						    			.andReturn();
+
+		//expected
+    	List<BoardEntity> expectedBoardEntitylist = new ObjectMapper().readValue(
+    			getClass().getClassLoader().getResourceAsStream(BOARD_JSON), new TypeReference<List<BoardEntity>>() {});
+    	expectedBoardEntitylist.add(givenBoardEntity);
     	
-    	for(int row = 0; row < expectedTable.getRowCount(); row++) {
-    		BoardEntity boardEntity = makeBoardEntity(expectedTable, row);
-    		expectList.add(boardEntity);
-    	}
-    	
-		return expectList;
+    	//then
+		assertThatJson(mockMVcResult.getResponse().getContentAsString())
+						.whenIgnoringPaths(JSON_IGNORE_ID, JSON_IGNORE_CREATE_AT, JSON_IGNORE_UPDATE_AT)
+						.when(Option.IGNORING_ARRAY_ORDER)
+						.isEqualTo(expectedBoardEntitylist);
 	}
 
-	public BoardEntity makeBoardEntity(ITable expectedTable, int row) throws DataSetException {
-		BoardEntity boardEntity = new BoardEntity();
-		boardEntity.setId(Integer.parseInt(expectedTable.getValue(row, ID).toString()));
-		boardEntity.setContents(expectedTable.getValue(row, CONTENTS).toString());
-		boardEntity.setUser_id(expectedTable.getValue(row, USER_ID).toString());
+	public BoardEntity makeGivenBoardEntity() {
+		BoardEntity givenBoardEntity = new BoardEntity();
+		givenBoardEntity.setUser_id("sunlike0303");
+		givenBoardEntity.setContents("호드를 위하여");
 		
-		return boardEntity;
+		return givenBoardEntity;
 	}
+
 }
